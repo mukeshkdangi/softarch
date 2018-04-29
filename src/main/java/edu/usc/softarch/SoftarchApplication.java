e10ef3b4d58ceb3f00e1a1b4513cb77834fba2df
Mukesh Kumar <mukesh.j.dangi@gmail.com>
2018-04-29 13:31:35 +0530

    Fist Soft Arch Prject

---
+++ src/main/java/edu/usc/softarch/SoftarchApplication.java
@@ -0,0 +1,12 @@
+package edu.usc.softarch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftarchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftarchApplication.class, args);
+	}
+}
