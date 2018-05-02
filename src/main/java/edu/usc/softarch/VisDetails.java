2f89fa2096f38098eb37adfdda8209f426125cbe
Mukesh Kumar <mukesh.j.dangi@gmail.com>
2018-05-02 16:00:15 +0530

    Changes for level 3

--- src/main/java/edu/usc/softarch/VisDetails.java
+++ src/main/java/edu/usc/softarch/VisDetails.java
@@ -16,18 +16,27 @@ public class VisDetails {
 
     private static String dep_fileName = "/Users/mukesh/Desktop/RELAX_log4j_2.7_1.0.0a/log4j-api_deps.rsf";
 
-     private static String clusters_fn = "/Users/mukesh/Desktop/RELAX_log4j_2.7_1.0.0a/log4j-api_relax_clusters_fn.rsf";
-   // public String dep_fileName;
-   // public String clusters_fn;
+    private static String clusters_fn = "/Users/mukesh/Desktop/RELAX_log4j_2.7_1.0.0a/log4j-api_relax_clusters_fn.rsf";
+    // public String dep_fileName;
+    // public String clusters_fn;
+    /**
+     * dependencMap : fileA -> {File B: File B type}
+     * clusterMap : File A name without path->> {File A Name without path, file A type}
+     */
+
     private Map<String, String> clusterMap = new HashMap<>();
-    private Map<String, List<String>> dependencMap = new HashMap<>();
+    private Map<String, Map<String, String>> dependencMap = new HashMap<>();
     private Map<String, Map<String, Integer>> levelOneMap = new HashMap<>();
 
+    /**
+     * @param depedency_file
+     * @param cluster_dep_file
+     */
 
     public void init(String depedency_file, String cluster_dep_file) {
 
-      //  this.dep_fileName = depedency_file;
-       // this.clusters_fn = cluster_dep_file;
+        //  this.dep_fileName = depedency_file;
+        // this.clusters_fn = cluster_dep_file;
 
 
         clusterMap = new HashMap<>();
@@ -35,7 +44,10 @@ public class VisDetails {
         levelOneMap = new HashMap<>();
     }
 
-    public Map<String, List<String>> createDependencyInfo() {
+    /**
+     * @return
+     */
+    public Map<String, Map<String, String>> createDependencyInfo() {
         try {
 
             File file = new File(dep_fileName);
@@ -47,17 +59,18 @@ public class VisDetails {
                 System.out.println(str);
 
                 String[] depenArray = str.split(" ");
-                List<String> depList;
+                Map<String, String> innerDepMap;
                 Map<String, String> typeCountMap;
 
                 if (dependencMap.get(depenArray[1]) == null) {
-                    depList = new ArrayList<>();
+                    innerDepMap = new HashMap<>();
                     typeCountMap = new HashMap<>();
                 } else {
-                    depList = dependencMap.get(depenArray[1]);
+                    innerDepMap = dependencMap.get(depenArray[1]);
                 }
-                depList.add(depenArray[2]);
-                dependencMap.put(depenArray[1], depList);
+                String fileTypeLocal = getFileTypeFromClusterMap(depenArray[2]);
+                innerDepMap.put(depenArray[2], fileTypeLocal);
+                dependencMap.put(depenArray[1], innerDepMap);
             });
 
         } catch (Exception e) {
@@ -66,7 +79,9 @@ public class VisDetails {
         return dependencMap;
     }
 
-
+    /**
+     * @return
+     */
     public Map<String, String> createClusterInfo() {
 
         try {
@@ -75,8 +90,6 @@ public class VisDetails {
 
 
             lines.stream().forEach(str -> {
-
-                System.out.println(str);
 
                 String[] depenArray = str.split(" ");
                 String filePath = depenArray[2];
@@ -90,13 +103,16 @@ public class VisDetails {
         return clusterMap;
     }
 
+    /**
+     * @return
+     */
     public Map<String, Map<String, Integer>> CreateLevelOneMap() {
 
         dependencMap.forEach((key, value) -> {
 
             String keyString = key;
             String fileName = keyString.substring(keyString.lastIndexOf(".") + 1);
-            List<String> depencyFiles = value;
+            Map<String, String> depencyFiles = value;
 
             String fileType = getFileTypeFromClusterMap(fileName);
 
@@ -114,10 +130,15 @@ public class VisDetails {
 
     }
 
-    private Map<String, Integer> updateFileTypeCount(List<String> depencyFiles, Map<String, Integer> typeCountMap) {
+    /**
+     * @param depencyFiles
+     * @param typeCountMap
+     * @return
+     */
+    private Map<String, Integer> updateFileTypeCount(Map<String, String> depencyFiles, Map<String, Integer> typeCountMap) {
 
 
-        depencyFiles.stream().forEach(file -> {
+        new ArrayList<String>(depencyFiles.keySet()).stream().forEach(file -> {
 
 
             file = file.substring(file.lastIndexOf(".") + 1);
@@ -132,6 +153,10 @@ public class VisDetails {
         return typeCountMap;
     }
 
+    /**
+     * @param fileName
+     * @return
+     */
     public String getFileTypeFromClusterMap(String fileName) {
         String type = "no_match";
         if (clusterMap.get(fileName) != null) {
